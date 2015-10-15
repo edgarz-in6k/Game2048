a16ac581e1bc2bbc78511277f6f055909b3b9388
employee <edgarz@interlink-ua.com>
2015-10-15 16:19:21 +0300

    create now

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -1,5 +1,12 @@
 package com;
 
+//import com.box.Field2048;
+
+import java.io.BufferedInputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello World!!!" );
