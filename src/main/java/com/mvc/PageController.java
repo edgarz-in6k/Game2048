5ae9109514c775b77b0cdedef17b9ca8187754cb
employee <edgarz@interlink-ua.com>
2015-11-13 17:16:59 +0200

    works game in browser

--- src/main/java/com/mvc/PageController.java
+++ src/main/java/com/mvc/PageController.java
@@ -1,20 +1,72 @@
 package com.mvc;
 
 import com.filler.RandomCellValueFiller;
+import com.game.Direction;
 import com.game.GameField;
 import com.generator.RandomCellValueGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class PageController {
 
-    @RequestMapping("/")
-    public String game(Model model){
-        GameField field = new GameField(new RandomCellValueFiller(new RandomCellValueGenerator()), 4);
+    public static final String GAME_FIELD = "gameField";
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String game(Model model, HttpServletRequest request){
+
+        HttpSession session = request.getSession();
+        GameField gameField = (GameField) session.getAttribute(GAME_FIELD);
+
+        if (gameField == null) {
+            gameField = new GameField(new RandomCellValueFiller(new RandomCellValueGenerator()), 4);
+            gameField.fillEmptyCell();
+            gameField.fillEmptyCell();
+            session.setAttribute(GAME_FIELD, gameField);
+        }
+
         model.addAttribute("hello", "Hello game!");
-        model.addAttribute("field", field);
+        model.addAttribute("key", "");
+        model.addAttribute(GAME_FIELD, gameField);
+        return "game";
+    }
+
+    @RequestMapping(value = "/{key}", method = RequestMethod.POST)
+    public String move(Model model, HttpServletRequest request, @PathVariable("key") String key){
+
+        HttpSession session = request.getSession();
+        GameField gameField = (GameField) session.getAttribute(GAME_FIELD);
+
+        System.out.println("!!!!!" + key);
+
+        switch (key) {
+            case "37":
+                gameField.move(Direction.LEFT);
+                gameField.fillEmptyCell();
+                break;
+            case "38":
+                gameField.move(Direction.UP);
+                gameField.fillEmptyCell();
+                break;
+            case "39":
+                gameField.move(Direction.RIGHT);
+                gameField.fillEmptyCell();
+                break;
+            case "40":
+                gameField.move(Direction.DOWN);
+                gameField.fillEmptyCell();
+                break;
+        }
+
+        model.addAttribute("hello", "Hello game!");
+        model.addAttribute("key", "");
+        model.addAttribute(GAME_FIELD, gameField);
         return "game";
     }
 
--- web/WEB-INF/pages/game.jsp
+++ web/WEB-INF/pages/game.jsp
@@ -1,5 +1,8 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <%@ page import="com.game.GameField" %>
+<%@ page import="com.game.Direction" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<% GameField gameField = (GameField) request.getAttribute("gameField"); %>
 <html>
 <head>
     <title></title>
@@ -8,13 +11,57 @@
     <h1>game.jsp</h1>
     <h1>${hello}</h1>
 
+    <div>
+        <%for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {%>
+                <%=gameField.getCell(i, j).getValue() + " "%>
+            <%}%>
+            </br>
+        <%}%>
+    </div>
+
+    <%--<form id="formSubmit" action="/move" method="POST" >
+        <input type="hidden" name="key" id="key" value="1"/>
+        <input type="submit" value="Submit"/>
+    </form>--%>
+    <form id="key" action="1" method="POST">
+        <input type="hidden" name="_method" value="POST">
+        <input type="submit">
+    </form>
+
     <script>
-        function print(){
-            var field = <%=((GameField)request.getAttribute("field")).getArray()%>;
-            for (var i = 0; i < 16; i++){
-                document.write(field[0])
+
+        window.onkeyup = function(e) {
+            var key = e.keyCode ? e.keyCode : e.which;
+            if ((key == 37) || (key == 38) || (key == 39) || (key == 40)){
+                var formSubmit = document.getElementById("key");
+                formSubmit.action = key;
+                formSubmit.submit();
             }
-            alert("!!!");
+
+            /*var xhr = new XMLHttpRequest();
+            xhr.open("GET", "/", true);
+            xhr.send(key);
+
+            if (xhr.status == 200){
+
+            }*/
+
+            <%--<%Direction direction;%>
+            switch (key){
+                case 37:
+                    <%direction = Direction.LEFT;%>
+                    break;
+                case 38:
+                    <%direction = Direction.UP;%>
+                    break;
+                case 39:
+                    <%direction = Direction.RIGHT;%>
+                    break;
+                case 40:
+                    <%direction = Direction.DOWN;%>
+                    break;
+            }--%>
         }
     </script>
 
